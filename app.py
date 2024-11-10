5b8b08e79ea4f8d187f037a828dbddb13aac3638
allenmylath <allengeorgemylath@gmail.com>
2024-11-10 13:01:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     
     print(f"Server starting on port {port}")
     uvicorn.run(
-        "server:app",
+        "app:app",
         host="0.0.0.0",
         port=port,
         reload=False if os.getenv("ENVIRONMENT") == "production" else True,
