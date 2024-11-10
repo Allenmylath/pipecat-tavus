e8beea66b091a58e1f43f5a3ece639a393a0e236
allenmylath <allengeorgemylath@gmail.com>
2024-11-10 12:56:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ async def shutdown_event():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "server:app",
+        "app:app",
         host="0.0.0.0",
         port=port,
         reload=True
